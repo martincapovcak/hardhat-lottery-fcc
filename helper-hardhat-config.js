d09ea4054f1b06e43ab209a9ccf07492a52865c9
Martin <martin.capovcak@gmail.com>
2022-08-18 00:10:11 +0200

    testing

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat")
 const networkConfig = {
     default: {
         name: "hardhat",
-        keepersUpdateInterval: "30",
+        interval: "30",
     },
     31337: {
         name: "localhost",
